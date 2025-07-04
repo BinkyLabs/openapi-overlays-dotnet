6b9aa806955f86d030e71007bd8d481793dc4cd4
kilifu <mcostabello@outlook.com>
2025-07-04 09:09:02 -0400

    docs: add error on missing documentation (#29)

--- src/Models/OverlayInfo.cs
+++ src/Models/OverlayInfo.cs
@@ -4,12 +4,28 @@ using Microsoft.OpenApi;
 
 namespace BinkyLabs.OpenApi.Overlays;
 
+/// <summary>
+/// Represents the Info object in the OpenAPI Overlay specification.
+/// </summary>
 public class OverlayInfo : IOverlaySerializable, IOverlayExtensible
 {
+    /// <summary>
+    /// Gets or sets the title of the overlay.
+    /// </summary>
     public string? Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the version of the overlay.
+    /// </summary>
     public string? Version { get; set; }
+
     /// <inheritdoc/>
     public IDictionary<string, IOverlayExtension>? Extensions { get; set; }
+
+    /// <summary>
+    /// Serializes the info object as an OpenAPI Overlay v1.0.0 JSON object.
+    /// </summary>
+    /// <param name="writer">The OpenAPI writer to use for serialization.</param>
     public void SerializeAsV1(IOpenApiWriter writer)
     {
         writer.WriteStartObject();
